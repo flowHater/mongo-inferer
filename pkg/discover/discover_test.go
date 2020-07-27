0c46ff2cfcda38e9185827dd8c09f802970b33f8
Pierrick Martin <pierrick@mart1.dev>
2020-07-27 23:07:02 +0200

    test wip

--- pkg/discover/discover_test.go
+++ pkg/discover/discover_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func TestLinkify(t *testing.T) {
@@ -14,21 +13,42 @@ func TestLinkify(t *testing.T) {
 		m           primitive.M
 		currentPath string
 	}
+
+	oid1 := primitive.NewObjectID()
+	oid2 := primitive.NewObjectID()
+
 	tests := []struct {
-		name    string
-		args    args
-		want    []Link
-		wantErr bool
+		name string
+		args args
+		want []Link
 	}{
-		// TODO: Add test cases.
+		{
+			name: "nominal case",
+			args: args{currentPath: "", m: primitive.M{"keyField": "valueField", "eeeeeId": oid1}},
+			want: []Link{{Path: "eeeeeId", Value: oid1.Hex()}},
+		}, {
+			name: "nested case",
+			args: args{currentPath: "", m: primitive.M{"keyField": "valueField", "eeeee": primitive.M{"testId": oid1}}},
+			want: []Link{{Path: "eeeee.testId", Value: oid1.Hex()}},
+		}, {
+			name: "multiple case",
+			args: args{currentPath: "", m: primitive.M{"keyField": "valueField", "eeeee1": oid1, "aaaaaaaa2": oid2}},
+			want: []Link{{Path: "eeeee1", Value: oid1.Hex()}, {Path: "aaaaaaaa2", Value: oid2.Hex()}},
+		}, {
+			name: "multiple nested case",
+			args: args{currentPath: "", m: primitive.M{"keyField": "valueField", "eeeee1": oid1, "aaaaaaaa2": oid2, "nested": primitive.M{"field1": oid1, "field2": oid2}}},
+			want: []Link{{Path: "eeeee1", Value: oid1.Hex()}, {Path: "aaaaaaaa2", Value: oid2.Hex()}, {Path: "nested.field1", Value: oid1.Hex()}, {Path: "nested.field2", Value: oid2.Hex()}},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := Linkify(tt.args.m, tt.args.currentPath)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Linkify() error = %v, wantErr %v", err, tt.wantErr)
+			if err != nil {
+				t.Errorf("Linkify() error = %v", err)
 				return
 			}
+
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Linkify() = %v, want %v", got, tt.want)
 			}
@@ -41,9 +61,8 @@ func TestDiscover_MatchLink(t *testing.T) {
 		repo Repo
 	}
 	type args struct {
-		ctx    context.Context
-		client *mongo.Client
-		ls     []Link
+		ctx context.Context
+		ls  []Link
 	}
 	tests := []struct {
 		name    string
@@ -52,7 +71,7 @@ func TestDiscover_MatchLink(t *testing.T) {
 		want    []Link
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		// {name: "nominal case", fields: fields{repo:}
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
